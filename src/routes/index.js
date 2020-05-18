f350d17a20ac4ef2c5b9c8c305296ad0d12e69c3
Vinicius <vinicius@iMac-de-Vinicius.local>
2020-05-18 20:40:23 -0300

    Implementação do loading antes de carregar a tela

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,35 @@
 import React, { useEffect, useState } from 'react';
 
+import { ActivityIndicator } from 'react-native';
 import firebase from '../services/firebase';
 
 import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
+import Layout from '../components/Layout';
 
 const Routes = () => {
   const [sign, setSign] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         setSign(true);
+        setLoading(false);
       } else {
         setSign(false);
       }
     });
   }, []);
+
+  if (loading) {
+    return (
+      <Layout justify="center">
+        <ActivityIndicator size="large" />
+      </Layout>
+    );
+  }
 
   return !sign ? <AuthRoutes /> : <AppRoutes />;
 };
