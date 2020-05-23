cd780e32b98825b92795434b497cab577b5adb4a
Vinicius <vinicius@iMac-de-Vinicius.local>
2020-05-23 12:04:36 -0300

    Refatoração do AppProvider, não necessitando mais importar os estados globais

--- src/components/AppProvider/index.js
+++ src/components/AppProvider/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import GloblaProvider from '../../contexts/GlobalContext';
 
 const AppProvider = ({ children, theme }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GloblaProvider>{children}</GloblaProvider>
+    </ThemeProvider>
+  );
 };
 
 export default AppProvider;
--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import theme from './theme';
 import { AppProvider } from './components';
-import GlobalProvider from './contexts/GlobalContext';
 
 import Routes from './routes';
 
 const App = () => {
   return (
     <AppProvider theme={theme}>
-      <GlobalProvider>
-        <NavigationContainer>
-          <Routes />
-        </NavigationContainer>
-      </GlobalProvider>
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
     </AppProvider>
   );
 };
